27309a2d60e627483253714bb34b96d097f58419
Oliver Nordbjerg <hi@notbjerg.me>
2019-08-28 18:46:01 +0200

    Fix worker WebSocket frame size

--- worker/src/provider.js
+++ worker/src/provider.js
@@ -2,6 +2,10 @@ import Web3 from 'web3'
 
 export default function createProvider () {
   return new Web3.providers.WebsocketProvider(
-    'wss://mainnet.eth.aragon.network/ws'
-  )
+    'wss://mainnet.eth.aragon.network/ws', {
+    clientConfig: {
+      maxReceivedFrameSize: 100000000,
+      maxReceivedMessageSize: 100000000
+    }
+  })
 }
