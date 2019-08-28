c4afba90943fed2829cb543c90eebcaa24e9117c
Oliver Nordbjerg <hi@notbjerg.me>
2019-08-28 18:50:57 +0200

    Fix lint

--- worker/src/provider.js
+++ worker/src/provider.js
@@ -3,9 +3,9 @@ import Web3 from 'web3'
 export default function createProvider () {
   return new Web3.providers.WebsocketProvider(
     'wss://mainnet.eth.aragon.network/ws', {
-    clientConfig: {
-      maxReceivedFrameSize: 100000000,
-      maxReceivedMessageSize: 100000000
-    }
-  })
+      clientConfig: {
+        maxReceivedFrameSize: 100000000,
+        maxReceivedMessageSize: 100000000
+      }
+    })
 }
