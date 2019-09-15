a6986ecaa75b66b00e7aad3f970be77b5b3153a9
Oliver Nordbjerg <hi@notbjerg.me>
2019-09-15 03:57:21 +0200

    Move filter state out of individual filters to filter container

--- website/src/components/Filter/DateRangeFilter.js
+++ website/src/components/Filter/DateRangeFilter.js
@@ -1,25 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React, { useCallback } from 'react'
 import PropTypes from 'prop-types'
 import { _DateRange as DateRange } from '@aragon/ui'
 
 export const FILTER_TYPE_DATE_RANGE = Symbol('FILTER_TYPE_DATE_RANGE')
 export function DateRangeFilter ({
   name,
+  value = {},
   onChange
 }) {
-  const [dateRange, setDateRange] = useState({})
-  useEffect(() => {
-    if (!dateRange.start || !dateRange.end) return
-
-    onChange(name, {
-      between: dateRange
-    })
-  }, [name, dateRange])
+  const setDateRange = useCallback((dateRange) => {
+    onChange(name, dateRange)
+  }, [name])
 
   return <DateRange
     onChange={setDateRange}
-    startDate={dateRange.start}
-    endDate={dateRange.end}
+    startDate={value.start}
+    endDate={value.end}
   />
 }
 
--- website/src/components/Filter/Filter.js
+++ website/src/components/Filter/Filter.js
@@ -1,30 +1,73 @@
-import React, { useState, useCallback, useEffect } from 'react'
+import React, { useState, useCallback, useMemo, useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { Button, GU } from '@aragon/ui'
+import qs from 'qs'
+import { Box, Button, GU } from '@aragon/ui'
 import { FILTER_TYPE_DATE_RANGE, DateRangeFilter } from './DateRangeFilter'
 import { FILTER_TYPE_LIST, ListFilter } from './ListFilter'
+import { useLocation } from '../../hooks/router'
 import { breakpoint } from '../../utils/breakpoint'
+
+function transformFilterValue (
+  filterType,
+  filterValue
+) {
+  switch (filterType) {
+    case FILTER_TYPE_DATE_RANGE:
+      return {
+        between: filterValue
+      }
+    case FILTER_TYPE_LIST:
+      return {
+        eq: filterValue
+      }
+    default:
+      throw new Error(`Unknown filter type ${filterType}`)
+  }
+}
 
 export function Filter ({
   filters = [],
   onChange
 }) {
-  const [filter, setFilter] = useState({})
+  const { location, navigate } = useLocation()
+  const [filterState, setFilterState] = useState(
+    qs.parse(location.search.slice(1))
+  )
 
-  const setFilterValue = useCallback((key, value) => {
-    setFilter((currentFilter) => ({
+  const setFilterValue = useCallback((filterName, filterValue) => {
+    setFilterState((currentFilter) => ({
       ...currentFilter,
-      [key]: value
+      [filterName]: filterValue
     }))
   })
 
-  useEffect(() => {
-    onChange(filter)
-  }, [filter])
+  const transformedFilter = useMemo(() => {
+    return Object.keys(filterState).reduce((f, filterName) => {
+      const filter = filters.find(
+        (filter) => filter.name === filterName
+      )
 
-  const hasFilter = !!Object.keys(filter).length
+      if (!filter) return
+
+      f[filter.name] = transformFilterValue(filter.type, filterState[filter.name])
+
+      return f
+    }, {})
+  }, [filters.length, filterState])
+
+  useEffect(() => {
+    onChange(transformedFilter)
+  }, [transformedFilter])
+
+  useEffect(() => {
+    navigate({
+      search: qs.stringify(filterState)
+    }, { replace: true })
+  }, [filterState])
+
+  const isFilterNotEmpty = !!Object.keys(filterState).length
   const clearFilter = useCallback(() => {
-    setFilter({})
+    setFilterState({})
   }, [])
 
   const columns = Math.max(
@@ -42,32 +85,34 @@ export function Filter ({
       grid-template-rows: auto;
     `}
   `}>
-    {filters.map((filter) => {
-      switch (filter.type) {
-        case FILTER_TYPE_LIST:
-          return <ListFilter
-            name={filter.name}
-            onChange={setFilterValue}
-            items={filter.items}
-            placeholder={filter.placeholder}
-          />
-        case FILTER_TYPE_DATE_RANGE:
-          return <DateRangeFilter
-            name={filter.name}
-            onChange={setFilterValue}
-          />
-        default:
-          throw new Error(`Unknown filter type ${filter.type}`)
+      {filters.map((filter) => {
+        switch (filter.type) {
+          case FILTER_TYPE_LIST:
+            return <ListFilter
+              name={filter.name}
+              value={filterState[filter.name]}
+              onChange={setFilterValue}
+              items={filter.items}
+              placeholder={filter.placeholder}
+            />
+          case FILTER_TYPE_DATE_RANGE:
+            return <DateRangeFilter
+              name={filter.name}
+              value={filterState[filter.name]}
+              onChange={setFilterValue}
+            />
+          default:
+            throw new Error(`Unknown filter type ${filter.type}`)
+        }
+      })}
+      {isFilterNotEmpty &&
+        <Button
+          css={breakpoint('medium')`grid-column: ${columns};`}
+          onClick={clearFilter}
+        >
+          Clear
+        </Button>
       }
-    })}
-    {hasFilter &&
-      <Button
-        css={breakpoint('medium')`grid-column: ${columns};`}
-        onClick={clearFilter}
-      >
-        Clear
-      </Button>
-    }
   </div>
 }
 
