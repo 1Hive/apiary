f34c3d832569c22d46d4a95181cd32ccfcba2dd6
Oliver Nordbjerg <hi@notbjerg.me>
2020-01-14 02:08:09 +0100

    Clean up activity indexing a bit

--- worker/src/activity/persist.js
+++ worker/src/activity/persist.js
@@ -5,14 +5,12 @@ export function * persist (
   ctx,
   trace
 ) {
-  const getApp = (address) => {
-    return ctx.db.collection('orgs')
-      .find({
-        'apps.address': ctx.web3.utils.toChecksumAddress(address)
-      })
-      .limit(1)
-      .map(({ apps }) => apps)
-      .next()
+  const getApp = async (address) => {
+    const exists = await ctx.db.collection('orgs').find({
+      'apps.address': ctx.web3.utils.toChecksumAddress(address)
+    }).limit(1).hasNext()
+
+    return exists ? address : null
   }
 
   // Find every app and organization mentioned in this trace
@@ -22,14 +20,8 @@ export function * persist (
 
   // Find actions in the trace that are to known apps
   const actions = trace.actions.filter((action) => {
-    return !!appsInTrace.find(
-      (app) => app.address === action.to
-    )
-  }).map((action) => ({
-    from: action.from,
-    to: action.to,
-    data: action.input
-  }))
+    return appsInTrace.includes(action.to)
+  })
 
   // This was not a transaction sent to an app
   if (actions.length === 0) return
