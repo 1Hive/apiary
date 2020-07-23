8fefea046a1d7b7f10db045c4026c22b4f6e7c45
Oliver Nordbjerg <hi@notbjerg.me>
2020-07-23 14:16:30 +0200

    Add basic API based on The Graph
    This version of the API decorates aspects of the Aragon Connect
    subgraph, however it currently lacks some information, most
    notably:
    
    - The address of the kit used to create the organization,
      which is unavailable in the subgraph we are currently using.
    - The ENS name of the organization, which is unavailable in the
      subgraph.
    - A creation timestamp, which is unavailable in the subgraph.
    - An AUM metric which we will need to get from another
      subgraph.
    - The `updateProfile` mutation currently present in Apiary,
      however this is somewhat trivial to add.
    
    However, it does contain the ability to read existing profiles
    in our database.
    
    This is a major breaking change, since most of the schema changed,
    including the pagination and filtering mechanism, as well as
    a lot of the field names and types.

--- api/src/index.js
+++ api/src/index.js
@@ -1,16 +1,98 @@
-import { GraphQLServer } from 'graphql-yoga'
-import setupDb from './db'
-import typeDefs from './schema'
-import resolvers from './resolvers'
+const { ApolloServer } = require('apollo-server')
+const { HttpLink } = require('apollo-link-http')
+const fetch = require('node-fetch')
+const {
+  introspectSchema,
+  linkToExecutor,
+  wrapSchema,
+  stitchSchemas,
+  FilterRootFields
+} = require('graphql-tools')
+const DataLoader = require('dataloader')
+const { MongoClient } = require('mongodb')
 
-setupDb()
-  .then((db) => {
-    return new GraphQLServer({
-      typeDefs,
-      resolvers,
-      context: { db }
-    })
+async function connectToDatabase() {
+  const db = await MongoClient.connect(process.env.MONGO_URL)
+
+  return {
+    profiles: db.collection('profiles')
+  }
+}
+
+async function buildSchema(loaders) {
+  const aragonConnectExecutor = linkToExecutor(new HttpLink({
+    uri: process.env.GRAPH_ARAGON_CONNECT,
+    fetch
+  }))
+
+  // Pull Aragon Connect schema from introspection query
+  const schema = await introspectSchema(aragonConnectExecutor)
+
+  // Make it an executable schema
+  const aragonConnectSchema = wrapSchema({
+    schema,
+    executor: aragonConnectExecutor
   })
-  .then((server) => {
-    server.start({ port: process.env.PORT || 3000 })
+
+  // This defines a set of transforms that will
+  // make the Aragon Connect schema more useable
+  // for our use-case.
+  const aragonConnectSchemaTransforms = [
+    new FilterRootFields((_, rootField) => [
+      'organizations',
+      'organization',
+      'repos'
+    ].includes(rootField))
+  ]
+
+  // Local schema that extends the remote schema
+  const localTypeDefs = `
+    type Profile {
+      name: String
+      icon: String
+      links: [String]!
+      editors: [String]!
+      description: String
+    }
+
+    extend type Organization {
+      profile: Profile!
+    }
+  `
+
+  // Merge the remote schemas with the local type defs
+  return stitchSchemas({
+    subschemas: [{
+      schema: aragonConnectSchema,
+      transforms: aragonConnectSchemaTransforms,
+    }],
+    typeDefs: localTypeDefs,
+    resolvers: {
+      Organization: {
+        profile: {
+          selectionSet: `{ address }`,
+          async resolve(org) {
+            return loaders.profileLoader.load(org.address)
+          }
+        }
+      }
+    }
   })
+}
+
+connectToDatabase().then(async (db) => {
+  const profileLoader = new DataLoader((keys) => {
+    return db.profiles.find({ address: { $in: keys } }).toArray()
+  })
+  const schema = await buildSchema({
+    profileLoader
+  })
+
+  // Start the GraphQL server
+  const server = new ApolloServer({ schema })
+
+  server.listen().then(({ url }) => {
+    console.log(`Server listening at ${url}`)
+  })
+})
+
